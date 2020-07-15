ef27ffee7642ef6814d427021ff8da4684ee3dfc
1vanguard <1vanguard@mail.ru>
2020-07-15 23:36:30 +0300

    Some updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './header';
 
 const App = () => {
   return (
@@ -8,14 +8,6 @@ const App = () => {
       <Header />
     </div>
   );
-}
-
-const Header = () =>{
-  return (
-    <header className="App-header">
-      <h1>Sexy-Engine</h1>
-    </header>
-  )
 }
 
 export default App;
